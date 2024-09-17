90d714a7242a8955f018b83256ff5d08e896ddf1
Fernando_Rivera <Fernandoyra01@gmail.com>
2024-09-16 21:20:40 -0600

    Agregar archivos de proyecto.

---
+++ Graficador_1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using info.lundin.math;
+namespace Graficador_1{
+    public partial class Form1 : Form{
+        double xi, xf;
+        int n;
+        public Form1(){
+            InitializeComponent();
+            n = chart1.Width;
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e) => Close();
+
+        private void BtnFirma_Click(object sender, EventArgs e) => System.Diagnostics.Process.Start("Firma_2");
+
+        private void Btgraficar_Click(object sender, EventArgs e){
+            xi = double.Parse(tBxi.Text);
+            xf = double.Parse(tBxf.Text);
+
+            Graficas gr1 = new Graficas(n);
+            gr1.Graficador(xi, xf);
+
+            for (int k = 0; k < n; k++) {
+                chart1.Series["Series1"].Points.AddXY(gr1.X[k], gr1.Y[k]);
+            }
+        }
+    }
+}
---
+++ Graficador_1/Graficas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using info.lundin.math;
+namespace Graficador_1{
+    internal class Graficas{
+        private double xi, xf;
+        private int n;
+        private double[] x;
+        private double[] y;
+
+        public Graficas(int n) {
+            this.n = n;
+            x = new double[n];
+            y = new double[n];
+        }
+        public double[] X { 
+            set { x = value; }
+            get { return x; }
+        }
+        public double[] Y { 
+            set { y = value; }
+            get { return y; }
+        }
+        public double fu(double x) {
+            double r;
+            r = Math.Cos(x);
+            return r;
+        }
+        public void Graficador (double xi, double xf) {
+            double h;
+            h = (xf - xi) / n;
+            for (int k = 0; k < n; k++){
+                X[k] = xi + k * h;
+                Y[k] = fu(X[k]);
+            }
+        }
+
+    }
+}
